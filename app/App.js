da5c6def85dd335afa54a5e751480ab6e054c4c4
Ryan Munsch <munschcreative@gmail.com>
2019-02-18 20:54:04 -0500

    added fetch for current weather

--- app/App.js
+++ app/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoading: true,
+      key: '7a4b8df164a345058e3212057191802',
+      data: ''
+    }
+  }
+
+  componentDidMount() {
+    return fetch(`https://api.apixu.com/v1/current.json?key=7a4b8df164a345058e3212057191802&q=Boston`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          data: responseJson.location
+        })
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+
   render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )
+    }
     return (
       <View style={styles.container}>
         <View>
           <Text>Munsch Weather Studio</Text>
+          <Text>{this.state.data.name}</Text>
         </View>
       </View>
     );
